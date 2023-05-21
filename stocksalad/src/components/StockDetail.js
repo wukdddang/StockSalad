6e2ad34dd9c50d6e3efbf09e04372da9bc2d72ef
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-05-21 15:43:10 +0900

    apexchart 적용 완료

--- stocksalad/src/components/StockDetail.js
+++ stocksalad/src/components/StockDetail.js
@@ -1,36 +1,11 @@
 import React from "react";
-import { Line } from "react-chartjs-2";
+import ApexChart from "apexcharts";
+import ApexCandle from "./ApexCandle";
 
 function StockDetail(props) {
-  const data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        type: "line",
-        label: "Dataset 1",
-        borderColor: "rgb(54, 162, 235)",
-        borderWidth: 2,
-        data: [1, 2, 3, 4, 5],
-      },
-      {
-        type: "bar",
-        label: "Dataset 2",
-        backgroundColor: "rgb(255, 99, 132)",
-        data: [1, 2, 3, 4, 5, 6],
-        borderColor: "red",
-        borderWidth: 2,
-      },
-      {
-        type: "bar",
-        label: "Dataset 3",
-        backgroundColor: "rgb(75, 192, 192)",
-        data: [1, 2, 3, 4, 5, 6],
-      },
-    ],
-  };
   return (
     <div className="w-full">
-      <Line type="line" data={data} />
+      <ApexCandle />
     </div>
   );
 }
--- stocksalad/src/pages/Detail.js
+++ stocksalad/src/pages/Detail.js
@@ -7,7 +7,7 @@ function Detail(props) {
   console.log(product);
   return (
     <div className="w-full p-10 bg-white rounded-lg">
-      <h2 className="text-3xl font-bold text-left">{product}</h2>
+      <h2 className="mb-6 text-3xl font-bold text-left">{product}</h2>
       <StockDetail />
     </div>
   );

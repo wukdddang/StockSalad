31d3c080ae39612879558eab277da07c04cb0a5c
Myeongeum Yoo <gmyungum@gmail.com>
2023-06-04 01:49:45 +0900

    Post 요청 추가 (날짜 기준)

--- server/src/main/kotlin/com/github/funkyblues/stock_salad/controller/NewsController.kt
+++ server/src/main/kotlin/com/github/funkyblues/stock_salad/controller/NewsController.kt
@@ -1,8 +1,13 @@
 package com.github.funkyblues.stock_salad.controller
 
 import com.github.funkyblues.stock_salad.model.News
+import com.github.funkyblues.stock_salad.model.DateRangeRequest
 import com.github.funkyblues.stock_salad.util.MongoDBUtil
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,5 +16,15 @@ class NewsController {
     fun getAllNews(): List<News> {
         val mongoTemplate = MongoDBUtil.getMongoTemplate()
         return mongoTemplate.findAll(News::class.java)
+    }
+
+    @PostMapping("/news")
+    fun getNews(@RequestBody dateRange: DateRangeRequest): List<News> {
+        val mongoTemplate = MongoDBUtil.getMongoTemplate()
+        val startDateTime = "${dateRange.startDate} 00:00:00"
+        val endDateTime = "${dateRange.endDate} 23:59:59"
+        val criteria = Criteria.where("writeDateTime").gte(startDateTime).lte(endDateTime)
+        val query = Query.query(criteria)
+        return mongoTemplate.find(query, News::class.java)
     }
 }
---
+++ server/src/main/kotlin/com/github/funkyblues/stock_salad/model/DateRangeRequest.kt
@@ -0,0 +1,6 @@
+package com.github.funkyblues.stock_salad.model
+
+data class DateRangeRequest (
+    val startDate: String,
+    val endDate: String,
+)

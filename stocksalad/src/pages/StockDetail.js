245bf0917d1379e57467c7a784fa084b78baf660
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-04 13:47:40 +0900

    CORS: 크롬 설정 변경으로 처리

--- stocksalad/src/pages/StockDetail.js
+++ stocksalad/src/pages/StockDetail.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import ApexCandle from "../components/ApexCandle";
+import axios from "axios";
 
 function StockDetail() {
   let { product } = useParams();
+
+  let data = {
+    startDate: "2023-05-28",
+    endDate: "2023-05-31"
+  };
+
+  axios.post('http://secmobile.asuscomm.com:30604/news', data, {
+      headers: {
+          'Content-Type': 'application/json'
+      }
+  })
+  // .then((response) => {
+  //     console.log(response.data);
+  // })
+  // .catch((error) => {
+  //     console.error(error);
+  // });
+
   return (
     <div className="w-full h-full px-6 py-4 bg-white rounded-lg">
       <h2 className="mb-6 text-3xl font-bold text-left">{product}</h2>

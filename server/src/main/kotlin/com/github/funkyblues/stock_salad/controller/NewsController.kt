61ced5be4eebca6dc1302c1b0b932f61f3e6a9f4
Myeongeum Yoo <gmyungum@gmail.com>
2023-06-11 12:18:17 +0900

    Refactor : Removed 뉴스 API
    시스템에 영향을 줄 수 있는 불필요한 API 삭제

--- server/src/main/kotlin/com/github/funkyblues/stock_salad/controller/NewsController.kt
+++ server/src/main/kotlin/com/github/funkyblues/stock_salad/controller/NewsController.kt
@@ -8,23 +8,28 @@ import org.springframework.data.mongodb.core.query.Query
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class NewsController {
-    @GetMapping("/news")
-    fun getAllNews(): List<News> {
-        val mongoTemplate = MongoDBUtil.getMongoTemplate()
-        return mongoTemplate.findAll(News::class.java)
+    @GetMapping("/news/all")
+    fun getAllNews(): String {
+        return "This API is removed."
+//        val mongoTemplate = MongoDBUtil.getMongoTemplate()
+//        return mongoTemplate.findAll(News::class.java)
     }
 
-    @PostMapping("/news")
-    fun getNews(@RequestBody dateRange: DateRangeRequest): List<News> {
-        val mongoTemplate = MongoDBUtil.getMongoTemplate()
-        val startDateTime = "${dateRange.startDate} 00:00:00"
-        val endDateTime = "${dateRange.endDate} 23:59:59"
-        val criteria = Criteria.where("writeDateTime").gte(startDateTime).lte(endDateTime)
-        val query = Query.query(criteria)
-        return mongoTemplate.find(query, News::class.java)
+    @GetMapping("/news")
+    fun getNews(
+        @RequestParam startDate: String,
+        @RequestParam endDate: String): String {
+        return "This API is removed."
+//        val mongoTemplate = MongoDBUtil.getMongoTemplate()
+//        val startDateTime = "$startDate 00:00:00"
+//        val endDateTime = "$endDate 23:59:59"
+//        val criteria = Criteria.where("writeDateTime").gte(startDateTime).lte(endDateTime)
+//        val query = Query.query(criteria)
+//        return mongoTemplate.find(query, News::class.java)
     }
 }

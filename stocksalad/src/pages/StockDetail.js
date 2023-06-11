ce4b033ee0e997247739f33f52c2e322db2ade03
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-11 14:44:25 +0900

    Feat: Added 주식 추가/삭제 기능 + Design: Added 주식 추가/삭제 스타일

--- stocksalad/src/pages/StockDetail.js
+++ stocksalad/src/pages/StockDetail.js
@@ -1,33 +1,65 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import ApexCandle from "../components/ApexCandle";
-import axios from "axios";
 
 function StockDetail() {
+  const user = localStorage.getItem("ss_user");
   let { product } = useParams();
+  let [loginStatus, setLoginStatus] = useState(false);
+  let [hasStock, setHasStock] = useState(false);
 
-  let data = {
-    startDate: "2023-05-28",
-    endDate: "2023-05-31"
-  };
+  useEffect(() => {
+    if (!user) {
+      setLoginStatus(false);
+    } else {
+      setLoginStatus(true);
+      const parsedUser = JSON.parse(user);
+      checkStock(parsedUser);
+    }
+  }, []);
 
-  axios.post('http://secmobile.asuscomm.com:30604/news', data, {
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  })
-  // .then((response) => {
-  //     console.log(response.data);
-  // })
-  // .catch((error) => {
-  //     console.error(error);
-  // });
+  function checkStock(parsedUser) {
+    return setHasStock(parsedUser.stock.includes(product));
+  }
+  function deleteStock() {
+    const parsedUser = JSON.parse(localStorage.getItem("ss_user"));
+    parsedUser.stock = parsedUser.stock.filter((item) => item !== product);
+    localStorage.setItem("ss_user", JSON.stringify(parsedUser));
+    setHasStock(false);
+  }
+  function addStock() {
+    const parsedUser = JSON.parse(localStorage.getItem("ss_user"));
+    parsedUser.stock.push(product);
+    localStorage.setItem("ss_user", JSON.stringify(parsedUser));
+    setHasStock(true);
+  }
 
   return (
-    <div className="w-full h-full px-6 py-4 bg-white rounded-lg">
-      <h2 className="mb-6 text-3xl font-bold text-left">{product}</h2>
-      <ApexCandle />
-    </div>
+    <>
+      <div className="w-full h-full px-6 py-4 bg-white rounded-lg">
+        <h2 className="mb-6 text-4xl font-bold text-center">{product}</h2>
+        <ApexCandle />
+      </div>
+      {loginStatus ? (
+        hasStock ? (
+          <button
+            className="px-6 py-2 m-5 text-white transition bg-red-500 border-4 border-red-100 rounded-lg cursor-pointer hover:bg-red-600 hover:text-white hover:border-red-200"
+            onClick={deleteStock}>
+            관심 목록에서 제거하기
+          </button>
+        ) : (
+          <button
+            onClick={addStock}
+            className="px-6 py-2 m-5 text-white transition bg-blue-500 border-4 border-blue-100 rounded-lg cursor-pointer hover:bg-blue-600 hover:text-white hover:border-blue-200">
+            관심 목록에 추가하기
+          </button>
+        )
+      ) : (
+        <button className="px-6 py-2 m-5 text-white transition bg-blue-500 border-4 border-blue-100 rounded-lg cursor-pointer bg-blue hover:bg-blue-600 hover:border-blue-200">
+          <Link to="/user">관심 목록에 추가하기</Link>
+        </button>
+      )}
+    </>
   );
 }
 
